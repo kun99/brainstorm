0cfe46bd923a4d381f9f357b5dd94a73fd8e28bc
kun99 <kuns.km@gmail.com>
2024-03-26 17:13:20 +0700

    storing words in an array

--- brainstorm.py
+++ brainstorm.py
@@ -6,9 +6,12 @@ def brainstorm():
     try:
         with open('words_dictionary.json') as f:
             dictionary = json.load(f)
-            lines = sum(1 for line in dictionary)
-            print(lines)
+        lines = sum(1 for line in dictionary)
         
+        words = [None] * lines
+        for i, word in enumerate(dictionary):
+            words[i] = word
+        print(words[0])
     except Exception as e:
         print("Couldn't open dictionary json file.")
     
